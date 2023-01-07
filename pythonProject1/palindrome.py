772bd28fb164703e1b9072d5e510b6815ac405af
OorielMoreno <ooriel.moreno@gmail.com>
2023-01-07 12:03:25 +0200

    le solved

--- pythonProject1/palindrome.py
+++ pythonProject1/palindrome.py
@@ -4,24 +4,39 @@ def is_palindrome(number: int)->bool:
     int_list = list(map(int, str(number)))
     is_solved = False
     list_index = 0
+    first_iteration = 1
+
     if len(int_list) % 2 != 0:
         del int_list[round(len(int_list) / 2 - 1)]
-
-    while is_solved == False:
-        if int_list[list_index] != int_list[len(int_list -list_index)]:
+    if len(int_list) < 3:
+        return 0
+    while True:
+        if int_list[list_index] != int_list[len(int_list) - list_index - 1]:
             return False
         if list_index > len(int_list) / 2:
             break
         list_index += 1
+        first_iteration = 0
     return True
 
-    # for y in int_list:
-    #     if y != int_list[len(int_list)-y]:
-    #         return False
-    #     if y >= len(int_list/2):
-    #         break
-    # return True
+def biggest_palindrome()->int:
+    multi1 = 999
+    multi2 = 999
+    second_count_index = 999
+    increasing_trductor = 1
+    biggest_so_far = 0
+    while multi2 > 0:
+        while multi1 > 0:
+            if multi1 * multi2 > 0:
+                if is_palindrome(multi1 * multi2):
+                    if multi1 * multi2 > biggest_so_far:
+                        biggest_so_far = multi1 * multi2
+            multi1 -= 1
+        multi1 = second_count_index
+        second_count_index -= increasing_trductor
+        increasing_trductor += 1
+        multi2 -= 1
+    return biggest_so_far
 
-
-print(is_palindrome(60001))
+print(biggest_palindrome())
 
